cd19b420f441dfc8e1d0269d4e297357344667af
aayushgoyal443 <aayushgoyal443@gmail.com>
2021-09-15 19:32:49 +0530

    Client to server registration done

--- Assignment-2/client.py
+++ Assignment-2/client.py
@@ -1,4 +1,3 @@
-from server import handle_chatting
 import socket
 import select
 import sys
@@ -7,60 +6,97 @@ import time
 
 # Desktop/COL334-Computer-Networks/Assignment-2/
 
+#Constants
+SERVER_PORT = 1234
+MAX_MESSAGE_SIZE = 1024
+
 # Global variables
-threads = []
 
-def handle_chatting(client, s_name):
 
+def handle_reg_feedback(feedback, sock):
+    if (feedback[0] == "REGISTERED"):
+        if (feedback[1]  =="TOSEND"):
+            print("Registered to send")
+        elif (feedback[1] =="TORECV"):
+            print("Registered to recv")
+        return True
+    else:
+        if (feedback[1] =="100"):
+            print("Malformed username")
+            sock.close()
+        else:
+            print("Server:", "No user registered")
+        exit()
+
+def send_messages():
+    while True: 
+        continue
+
+def recv_messages():
     while True:
-        message = client.recv(1024)
-        message = message.decode()
-        print(s_name, ":", message)
-        message = input(str("Me : "))
-        if message == "[e]":
-            message = "Left chat room!"
-            client.send(message.encode())
-            print("\n")
-            break
-        client.send(message.encode())
+        continue
 
 
 def main():
 
-    print("\nWelcome to Chat Room\n")
-    print("Initialising....\n")
-    time.sleep(1)
-
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    try:
-        name = sys.argv[1]
-    except:
-        print("please pass username as command line argument")
+    if (len(sys.argv) != 3 ):
+        print("Provide username and server address as command line arguments")
         exit()
+    global username
+    username = sys.argv[1]
 
-    host = input(str("Enter server address: ")) 
-    port = 1234     # port of server
-    print("\nTrying to connect to ", host, "(", port, ")\n")
+    # host = input(str("Enter server address: ")) 
+    host = sys.argv[2]
+    port = SERVER_PORT     # port of server
 
-    time.sleep(1)
+    # Making two sockets for sending and receiving
 
+    #Opening the send socket
+    clientSEND = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSEND.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("\nTrying to connect SEND socket to",  host +"#" + str(port) + "\n")
     try:
-        client.connect((host, port))
-        print("Connected...\n")
+        clientSEND.connect((host, port))
     except:
-        print("Not able to connect...try again :)")
-        exit()
+        print("Not able to connect to server...try again")
+    # Registering the SEND socket
+    message = "REGISTER TOSEND " +str(username) + "\n\n"
+    clientSEND.send(message.encode())
+    feedback = clientSEND.recv(MAX_MESSAGE_SIZE)
+    feedback = feedback.decode().split()
+    registered_sender = handle_reg_feedback(feedback, clientSEND)
 
-    client.send(name.encode())
-    s_name = client.recv(1024)
-    s_name = s_name.decode()
-    print(s_name, "has joined the chat room\nEnter [e] to exit chat room\n")
 
-    t=  threading.Thread(target= handle_chatting, args = [client, s_name])
-    threads.append(t)
-    t.start()
+    #Opening the Receive socket
+    clientRECV = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientRECV.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("\nTrying to connect RECV socket to ", host, "(", port, ")\n")
+    try:
+        clientRECV.connect((host, port))
+    except:
+        print("Not able to connect to server...try again")
+    # Registering the RECV socket
+    message = "REGISTER TORECV " + str(username) + "\n\n"
+    clientRECV.send(message.encode())
+    feedback = clientRECV.recv(MAX_MESSAGE_SIZE)
+    feedback = feedback.decode().split()
+    registered_receiver = handle_reg_feedback(feedback, clientRECV)
+
+
+    if (registered_sender and registered_receiver):
+        print("\Welcome to chatroom\n")
+
+
+    global threadSEND, threadRECV
+    threadSEND= threading.Thread(target= send_messages, args = [])
+    threadRECV= threading.Thread(target= recv_messages, args = [])
+    threadSEND.start()
+    threadRECV.start()
+
+    if (threadSEND.is_alive()):
+        threadSEND.join()
+    if (threadRECV.is_alive()):
+        threadRECV.join()
 
 
 if __name__ == "__main__":
--- Assignment-2/server.py
+++ Assignment-2/server.py
@@ -5,58 +5,103 @@ import threading
 import time
 
 # Constants
-MAX_CLIENTS = 10
-SERVER_NAME = 'assyush'
+MAX_CLIENTS = 16
+SERVER_PORT = 1234
+MAX_MESSAGE_SIZE = 1024
+
+# Global Variables
+registered_senders = {}
+registered_receivers = {}
+
+def isRegistered(username):
+    if (username in registered_receivers) and (username in registered_senders):
+        return True
+    else:
+        return False
+
+# conn.send(message.encode())
+# message = message.decode()
 
 
-def handle_chatting(conn, addr, s_name):
+# To check if a username if valid or not
+def valid_username(username):
+    for c in username:
+        if ( 'a' <= c <= 'z'): 
+            continue
+        elif ('A' <= c <= 'Z'):
+            continue
+        elif ('0' <= c <= '9'):
+            continue
+        else:
+            return False
+    return True
+
+
+def serve_client(conn, addr):
     while True:
-        message = input(str("Me : "))
-        if message == "[e]":
-            message = "Left chat room!"
-            conn.send(message.encode())
-            print("\n")
-            break
-        conn.send(message.encode())
-        message = conn.recv(1024)
-        message = message.decode()
-        print(s_name, ":", message)
+        continue
+    
+
+
+def handle_client_reg(conn, addr):
+    message = conn.recv(MAX_MESSAGE_SIZE)
+    message = message.decode()
+    task = message.split()
+    fault = False
+    username = task[2]
+    if ( len(task)!=3 or ((task[0] != "REGISTER" or (task[1]!= "TOSEND"  and task[1]!= "TORECV" )) and (not isRegistered(username)))):
+        feedback = f"ERROR 101 No user registered\n\n"
+        fault = True
+    if ( (not fault) and (not valid_username(username)) ):
+        feedback =  f'ERROR 100 Malformed username\n\n'
+        fault = True
+    
+    if (fault):
+        conn.send(feedback.encode())
+        conn.close()
+        return
+
+    # All good, no fault
+    if (task[1] == "TORECV"):
+        registered_receivers[username] = (conn, addr)
+        feedback = 'REGISTERED TORECV '+ str(username) + '\n\n'
+    else:
+        registered_senders[username] = (conn, addr)
+        feedback = 'REGISTERED TOSEND '+ str(username) + '\n\n'
+    conn.send(feedback.encode())
+    print(f"{username} has registered in the server")
+
+    if (task[1] == "TOSEND"):
+        serve_client(conn, addr)
+    else:
+        print("RECV thread closed")
+
 
 def main():
-    print("\nWelcome to Chat Room\n")
-    time.sleep(1)
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # this will form a TCP socket
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     host = socket.gethostname()
     ip = socket.gethostbyname(host)     # this is the IP address of our Server, client must pass this as server address in order to connect
-    port = 1234     # this should be free
+    port = SERVER_PORT     # this should be free
     server.bind((host, port))
     print(host, "(", ip, ")\n")
     server.listen(MAX_CLIENTS)
 
-    clients = []    # each of them will be a pair, containing both the SEND and RECV sockets
-
-
     print("\nWaiting for incoming connections...\n")
-
-    threads = []
 
     while True:
         conn, addr = server.accept()
         print("Received connection from ", addr[0], "(", addr[1], ")\n")
-
-        s_name = conn.recv(1024)
-        s_name = s_name.decode()
-        conn.send(SERVER_NAME.encode())
-        print(s_name, "has connected to the chat room\nEnter [e] to exit chat room\n")
-
         # Now i think it is the time to chat with them on a separate thread
-        t = threading.Thread(target= handle_chatting , args= [conn, addr, s_name])  
+        t = threading.Thread(target= handle_client_reg , args= [conn, addr])
         t.start()
-        threads.append(t)
-        
+    
+    # for thread in threads:
+    #     if (thread.is_alive()):
+    #         thread.join()
+
 
 if __name__ == '__main__':
     main()
