a583b742bd255bfe20823e3b4733e484a4288eda
aayushgoyal443 <aayushgoyal443@gmail.com>
2021-09-16 11:59:57 +0530

    unicast and broadcast working | need to check for errors

--- Assignment-2/client.py
+++ Assignment-2/client.py
@@ -12,6 +12,16 @@ MAX_MESSAGE_SIZE = 1024
 
 # Global variables
 
+def content_length(msg):
+    return len(msg)
+
+
+def disconnect():
+    clientRECV.close()
+    clientSEND.close()
+    global connected
+    connected = False
+
 
 def handle_reg_feedback(feedback, sock):
     if (feedback[0] == "REGISTERED"):
@@ -28,13 +38,97 @@ def handle_reg_feedback(feedback, sock):
             print("Server:", "No user registered")
         exit()
 
+def valid_msg_format(msg):
+    valid = True
+    if (len(msg)<4):
+        valid = False
+    elif (msg[0]!= '@'):
+        valid = False
+    elif (msg[1] == ' '):
+        valid = False
+    if (valid):
+        for (i,c) in enumerate(msg):
+            if (c ==' ' and i< len(msg)-1 and msg[i+1]!= ' '):
+                return True
+    print('chat app: invalid message format')
+    return False
+
+
+def parse_message(msg):
+    idx = -1
+    for (i,c) in enumerate(msg):
+        if (c ==' '):
+            idx  = i
+            break
+    recipient =  msg[1:idx]
+    message = msg[idx+1:]     # this will give us the username by removing the @ from it
+    return recipient, message
+
+
+def handle_send_feedback(feedback):
+    if (feedback[0] == "SENT"):
+        return True
+    
+    # We encountered some error 
+    if (feedback[0] == "ERROR"):
+        if (feedback[1] == "102"):
+            print("server: Recipient is not registered")
+            return True
+        elif (feedback[1] == '103'):
+            print('server: Header incomplete')
+            return False
+
+
 def send_messages():
-    while True: 
-        continue
+    while connected:
+        inp = input()
+        inp  = inp.strip()      # for cleaning irrelevant newlines at start and end of message
+        if (not valid_msg_format(inp)):
+            continue
+        recipient, msg =  parse_message(inp)
+        message = f"SEND {recipient}\nContent-length: {content_length(msg)}\n\n{msg}"
+        clientSEND.send(message.encode())
+        #Now waiting for a response
+
+        feedback = clientSEND.recv(MAX_MESSAGE_SIZE)
+        feedback = feedback.decode().split()
+        keep_running = handle_send_feedback(feedback)
+        if (not keep_running):
+            disconnect()
+
+
+def is_well_formed(info):
+    # check if ERROR 103 needs to be raised or not
+    invalid = False
+    if (len(info)!= 4):
+        invalid = True
+    elif (info[2] != ''):
+        invalid = True
+    else:
+        header_info  = info[1].split(': ')
+        if (len(header_info) != 2):
+            invalid = True
+        elif (header_info[0]!= "Content-length"):
+            invalid = True
+        elif ( len(info[3]) != int(header_info[1])):
+            invalid = True
+
+    return (not invalid)
 
 def recv_messages():
-    while True:
-        continue
+    while connected:
+        info = clientRECV.recv(MAX_MESSAGE_SIZE)
+        info = info.decode().split('\n')
+        if (not is_well_formed(info)):
+            fd = f"ERROR 103 Header Incomplete\n\n"
+            print("Incomplete header from server")
+            clientRECV.send(fd.encode())
+            disconnect()
+        else:
+            sender = info[0].split()[1]
+            fd = f"RECEIVED {info}\n\n"
+            clientRECV.send(fd.encode())
+            print(f"{sender}: {info[3]}")
 
 
 def main():
@@ -50,22 +144,7 @@ def main():
     port = SERVER_PORT     # port of server
 
     # Making two sockets for sending and receiving
-
-    #Opening the send socket
-    clientSEND = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSEND.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    print("\nTrying to connect SEND socket to",  host +"#" + str(port) + "\n")
-    try:
-        clientSEND.connect((host, port))
-    except:
-        print("Not able to connect to server...try again")
-    # Registering the SEND socket
-    message = "REGISTER TOSEND " +str(username) + "\n\n"
-    clientSEND.send(message.encode())
-    feedback = clientSEND.recv(MAX_MESSAGE_SIZE)
-    feedback = feedback.decode().split()
-    registered_sender = handle_reg_feedback(feedback, clientSEND)
-
+    global clientSEND, clientRECV
 
     #Opening the Receive socket
     clientRECV = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,6 +154,7 @@ def main():
         clientRECV.connect((host, port))
     except:
         print("Not able to connect to server...try again")
+    print(clientRECV)
     # Registering the RECV socket
     message = "REGISTER TORECV " + str(username) + "\n\n"
     clientRECV.send(message.encode())
@@ -82,9 +162,25 @@ def main():
     feedback = feedback.decode().split()
     registered_receiver = handle_reg_feedback(feedback, clientRECV)
 
+    #Opening the send socket
+    clientSEND = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSEND.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    print("\nTrying to connect SEND socket to",  host +"#" + str(port))
+    try:
+        clientSEND.connect((host, port))
+    except:
+        print("Not able to connect to server...try again")
+    # Registering the SEND socket
+    message = f"REGISTER TOSEND {username} \n\n"
+    clientSEND.send(message.encode())
+    feedback = clientSEND.recv(MAX_MESSAGE_SIZE)
+    feedback = feedback.decode().split()
+    registered_sender = handle_reg_feedback(feedback, clientSEND)
 
-    if (registered_sender and registered_receiver):
-        print("\Welcome to chatroom\n")
+    global connected
+    connected = registered_sender and registered_receiver
+    if (connected):
+        print("\n------------------- Welcome to chatroom -------------------\n")
 
 
     global threadSEND, threadRECV
--- Assignment-2/server.py
+++ Assignment-2/server.py
@@ -12,15 +12,26 @@ MAX_MESSAGE_SIZE = 1024
 # Global Variables
 registered_senders = {}
 registered_receivers = {}
+feedbacks = []      # for storing feedbacks from broadcast, since threading can call only non-void functions
 
-def isRegistered(username):
-    if (username in registered_receivers) and (username in registered_senders):
-        return True
-    else:
+def content_length(msg):
+    return len(msg)
+
+def disconnect_user(username):
+    if (username in registered_senders):
+        registered_senders[username][0].close()
+        registered_senders.pop(username)
+    if (username in registered_receivers):
+        registered_receivers[username][0].close()
+        registered_receivers.pop(username)
+
+
+def is_registered(username):
+    if username not in registered_receivers:
         return False
-
-# conn.send(message.encode())
-# message = message.decode()
+    if username not in registered_senders:
+        return False
+    return True
 
 
 # To check if a username if valid or not
@@ -36,20 +47,95 @@ def valid_username(username):
             return False
     return True
 
+def broadcast(sender, msg):
+    global feedbacks
+    feedbacks = []
+    ts = []
+    i=0
+    for recipient in registered_receivers:
+        if (recipient == sender):
+            continue
+        feedbacks.append("")
+        t = threading.Thread(target= unicast, args = [recipient, sender,  msg, i])
+        t.start()
+        ts.append(t)
+        i+=1
 
-def serve_client(conn, addr):
-    while True:
-        continue
+    for t in ts:
+        t.join()
+    for fd in feedbacks: 
+        if (fd == "ERROR 102 Unable to send\n\n"):
+            return fd
+    return "SENT ALL"
     
 
+def unicast(recipient, sender, msg, idx =-1):
+    if (recipient not in registered_receivers):
+        feedback = "ERROR 102 Unable to send\n\n"
+        return feedback
+    # The recipent is registered and hence we should send them message
+    message = f"FORWARD {sender}\nContent-length: {content_length(msg)}\n\n{msg}"
+    registered_receivers[recipient][0].send(message.encode())
+    feedback = registered_receivers[recipient][0].recv(MAX_MESSAGE_SIZE)
+    feedback = feedback.decode().split()
+    if (feedback[0] == "ERROR" and feedback[1] == "103"):
+        disconnect_user(recipient)
+        fd = "ERROR 102 Unable to send\n\n"
+    else: 
+        # it would have got the "RECEIVED"
+        fd = "SENT {recipient}\n\n"
+    if (idx!=-1):
+        feedbacks[idx] = fd
+    return fd
+
+
+def is_well_formed(info):
+    # check if ERROR 103 needs to be raised or not
+    invalid = False
+    if (len(info)!= 4):
+        invalid = True
+    elif (info[2] != ''):
+        invalid = True
+    else:
+        header_info  = info[1].split(': ')
+        if (len(header_info) != 2):
+            invalid = True
+        elif (header_info[0]!= "Content-length"):
+            invalid = True
+        elif ( len(info[3]) != int(header_info[1])):
+            invalid = True
+
+    return (not invalid)
+
+
+def serve_client(conn, addr, username):
+    while is_registered(username):
+        message= conn.recv(MAX_MESSAGE_SIZE)
+        info = message.decode().split('\n')
+        if not is_well_formed(info):
+            feedback = f"ERROR 103 Header incomplete\n\n"
+            conn.send(feedback.encode())
+            disconnect_user(username)
+            continue
+        recipient = info[0].split()[1]
+        if (recipient == "ALL"):
+            feedback = broadcast(username, info[3])
+        else:
+            feedback = unicast(recipient,username, info[3])
+        feedback= feedback.split()
+        if (feedback[0] == 'SENT'):
+            feedback  = f"SENT {recipient}"
+        else:
+            feedback = f"ERROR 102 Unable to send"
+        
+        conn.send(feedback.encode())
 
 def handle_client_reg(conn, addr):
     message = conn.recv(MAX_MESSAGE_SIZE)
-    message = message.decode()
-    task = message.split()
+    task = message.decode().split()
     fault = False
     username = task[2]
-    if ( len(task)!=3 or ((task[0] != "REGISTER" or (task[1]!= "TOSEND"  and task[1]!= "TORECV" )) and (not isRegistered(username)))):
+    if ( len(task)!=3 or ((task[0] != "REGISTER" or (task[1]!= "TOSEND"  and task[1]!= "TORECV" )) and (not is_registered(username)))):
         feedback = f"ERROR 101 No user registered\n\n"
         fault = True
     if ( (not fault) and (not valid_username(username)) ):
@@ -72,7 +158,7 @@ def handle_client_reg(conn, addr):
     print(f"{username} has registered in the server")
 
     if (task[1] == "TOSEND"):
-        serve_client(conn, addr)
+        serve_client(conn, addr, username)
     else:
         print("RECV thread closed")
 
@@ -86,7 +172,7 @@ def main():
     ip = socket.gethostbyname(host)     # this is the IP address of our Server, client must pass this as server address in order to connect
     port = SERVER_PORT     # this should be free
     server.bind((host, port))
-    print(host, "(", ip, ")\n")
+    print(host, "(", ip, ")")
     server.listen(MAX_CLIENTS)
 
     print("\nWaiting for incoming connections...\n")
