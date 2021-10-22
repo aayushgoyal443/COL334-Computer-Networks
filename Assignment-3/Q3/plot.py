a178a87f3b01bf0bd2fc25d8ea9eac1d14ba69a0
aayushgoyal443 <aayushgoyal443@gmail.com>
2021-10-23 03:40:59 +0530

    plotting script for 3rd question

---
+++ Assignment-3/Q3/plot.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import sys
+
+FILE_NAME =  sys.argv[1]
+
+A = np.genfromtxt(FILE_NAME,dtype =  None)
+time = []
+size = []
+
+for t in A:
+    time.append(t[0])
+    size.append(int(t[1]))
+
+time = np.array(time)
+size = np.array(size)
+
+plt.plot(time, size)
+plt.xlabel("Time (in s)")
+plt.ylabel("Congestion window size")
+FILE_NAME = FILE_NAME.strip(".cwnd").split('/')[-1]
+
+plt.title(FILE_NAME)
+plt.savefig(f"plots/{FILE_NAME}.png")
